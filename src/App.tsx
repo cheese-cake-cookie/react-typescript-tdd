498c65c17b0379ba97b6df2d779e57c27e6cd483
wasabi <wasabi@podbbang.com>
2021-10-26 16:56:31 +0900

    feat: Code with dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
+export type TodoState = 'BACKLOG' | 'INPROGRESS' | 'DONE';
+export interface Todo {
+  id: number,
+  title: string,
+  state: TodoState,
+}
+
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [title, setTitle] = useState('');
+
+  useEffect(() => {
+    getTodos();
+  }, [])
+
+  const getTodos = async () => {
+    const { data }: {data: { data: Todo[] }} = await axios.get('/json/todo.json');
+    setTodos(data.data);
+  }
+
+  const addTodo = (title: string) => {
+    setTodos([...todos, {
+      id: todos.length + 1,
+      title: title,
+      state: 'BACKLOG'
+    }]);
+  }
+
+  const deleteTodo = (id: number) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  }
+  
+  const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTitle(e.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text"
+        value={title}
+        onChange={onChangeHandler}
+        onKeyUp={e => {
+        if (e.key === 'Enter') {
+          addTodo(title);
+          setTitle('');
+        }
+      }} placeholder="press enter to create todo" />
+
+      {
+        todos.map(todo => (
+            <li key={ todo.id }>
+              {todo.title}
+            {todo.state}
+            <button onClick={e => {
+              deleteTodo(todo.id)
+            }}>del</button>
+            </li>
+          )
+        )
+      }
     </div>
   );
 }
