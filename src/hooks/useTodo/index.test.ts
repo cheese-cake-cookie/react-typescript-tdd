d424094857a4d91b223baf4397b415e648f805de
wasabi <wasabi@podbbang.com>
2021-10-27 10:50:30 +0900

    feat: Set test code - remove todo

--- src/hooks/useTodo/index.test.ts
+++ src/hooks/useTodo/index.test.ts
@@ -28,7 +28,7 @@ const mock = new MockAdapter(axios);
 describe("TODO APP", () => {
   test('add todo', async () => {
     // parameter of onGet method is actual API URL but response value is mocked data.
-    mock.onGet('/json/todo.json').reply(200, { data: TEMP_TODOS });
+    mock.onGet('/json/todos.json').reply(200, { data: TEMP_TODOS });
 
     // simulate rendering by @testing-library/react-hooks
     const { result, rerender, waitForNextUpdate } = renderHook(() => useTodo());
@@ -41,5 +41,46 @@ describe("TODO APP", () => {
 
     act(() => result.current.addTodo(TEMP_TODOS[1].title));
     expect(result.current.todos.length).toBe(5);
+  })
+});
+
+describe('remove todo', () => {
+  let hooks: {
+    current: {
+      todos: Todo[],
+      addTodo: (title: string) => Promise<void | undefined>,
+      deleteTodo: (idx: number) => Promise<void | undefined>,
+    }
+  };
+
+  // The process launch before running test();
+  beforeEach(async () => {
+    mock.onGet('/json/todos.json').reply(200, { data: TEMP_TODOS });
+
+    const { result, waitForNextUpdate }: any = renderHook(() => useTodo());
+    hooks = result;
+
+    await waitForNextUpdate();
+
+    // add 3 more todos
+    act(() => hooks.current.addTodo(TEMP_TODOS[0].title));
+    act(() => hooks.current.addTodo(TEMP_TODOS[1].title));
+    act(() => hooks.current.addTodo(TEMP_TODOS[2].title));
+  });
+
+  test('remove first item', () => {
+    expect(hooks.current.todos.length).toBe(6);
+
+    act(() => hooks.current.deleteTodo(1));
+
+    expect(hooks.current.todos.length).toBe(5);
+  });
+
+  test('remove last item', () => {
+    expect(hooks.current.todos.length).toBe(6);
+    
+    act(() => hooks.current.deleteTodo(6));
+
+    expect(hooks.current.todos.length).toBe(5);
   })
 });
